f4575da458ff34ef73a9b7261172a5e2607d65c1
fsojudi@gmail.com <fsojudi@gmail.com>
2022-02-16 23:55:07 +0100

    Add project files.

---
+++ Hangman/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nameArray = new string[2] { "sara", "Zohreh" };
+            string name1 = nameArray[0];
+            
+            char[] letterArray = new char[name1.Length];
+            
+           List<char> guess = new List< char>();
+
+            for (int i = 0; i < name1.Length; i++)
+            {
+                letterArray[i] = name1[i];
+
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                guess[i] = char.Parse (Console.ReadLine());
+
+
+                foreach (char letter in letterArray)
+                {
+                    if (guess[i]== letter )
+
+                    {
+                        Console.WriteLine(" good");
+
+                    }
+    ;
+                }
+            }
+           
+                   
+        
+        }
+       
+
+    }
+}
