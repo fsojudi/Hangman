42c6872b518d4789fc3b71d9792cc093416e4869
fsojudi@gmail.com <fsojudi@gmail.com>
2022-02-17 21:33:27 +0100

    Assignment Hangman

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -7,39 +7,89 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
-            string[] nameArray = new string[2] { "sara", "Zohreh" };
-            string name1 = nameArray[0];
-            
-            char[] letterArray = new char[name1.Length];
-            
-           List<char> guess = new List< char>();
-
-            for (int i = 0; i < name1.Length; i++)
+            string[] nameArray = new string[3] { "sara", "zohreh", "kiana" };
+            string name = nameArray[1];
+            Console.WriteLine("\nThe length of the name is : ");
+            foreach (char x in name)
             {
-                letterArray[i] = name1[i];
 
+                Console.Write("_ ");
             }
-            for (int i = 0; i < 10; i++)
+
+            int nameLength = name.Length;
+            int amountOfTimesWrong = 0;
+            List<char> currentLettersGuessed = new List<char>();
+            int currentLettersRight = 0;
+
+            while (amountOfTimesWrong != 10 && currentLettersRight != nameLength)
             {
-                guess[i] = char.Parse (Console.ReadLine());
 
 
-                foreach (char letter in letterArray)
+                Console.Write("\n input your guessed letter: ");
+                char letterGuessed = Console.ReadLine()[0];
+
+
+                //  chek if guessed letter is right
+                bool right = false;
+                for (int i = 0; i < name.Length; i++)
+
                 {
-                    if (guess[i]== letter )
-
+                    if (letterGuessed == name[i])
                     {
-                        Console.WriteLine(" good");
-
+                        right = true;
                     }
-    ;
                 }
+
+                // if User was right
+                if (right)
+                {
+                    currentLettersGuessed.Add(letterGuessed);
+                    currentLettersRight = printname(currentLettersGuessed, name);
+                    Console.Write("\r\n");
+
+                }
+                // if user was wrong
+                else
+                {
+                    amountOfTimesWrong += 1;
+                    currentLettersGuessed.Add(letterGuessed);
+
+                    currentLettersRight = printname(currentLettersGuessed, name);
+                    Console.Write("\r\n");
+
+                }
+
             }
-           
-                   
-        
+            Console.WriteLine("\r\nGame over!");
+
+
         }
-       
+
+
+
+
+        private static int printname(List<char> guessLetter, String secretword)
+        {
+            int counter = 0;
+            int rightLetter = 0;
+            foreach (char letter in secretword)
+            {
+                if (guessLetter.Contains(letter))
+                {
+                    Console.Write("\r\n");
+                    Console.WriteLine(letter + "");
+                    rightLetter += 1;
+                }
+                else
+                {
+                    Console.WriteLine(" ");
+
+                }
+                counter += 1;
+
+            }
+            return rightLetter;
+        }
 
     }
 }
